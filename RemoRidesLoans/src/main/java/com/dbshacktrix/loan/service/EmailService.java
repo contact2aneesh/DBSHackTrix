8b442562d9ad67324bbdd5bc2943c99b3d06d21e
Aneesh Bansal <contact2aneesh@gmail.com>
2019-03-30 13:20:06 +0530

    Code for email service

---
+++ RemoRidesLoans/src/main/java/com/dbshacktrix/loan/service/EmailService.java
@@ -0,0 +1,37 @@
+package com.dbshacktrix.loan.service;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailService {
+	
+	public void sendmail(String to, String subject, String body) throws AddressException, MessagingException, IOException {
+		   Properties props = new Properties();
+		   props.put("mail.smtp.auth", "true");
+		   props.put("mail.smtp.starttls.enable", "true");
+		   props.put("mail.smtp.host", "smtp.gmail.com");
+		   props.put("mail.smtp.port", "587");
+		   
+		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+		      protected PasswordAuthentication getPasswordAuthentication() {
+		         return new PasswordAuthentication("table10.dbshacktricks@gmail.com", "aneesh54321");
+		      }
+		   });
+		   Message msg = new MimeMessage(session);
+		   msg.setFrom(new InternetAddress("table10.dbshacktricks@gmail.com", false));
+
+		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+		   msg.setSubject(subject);
+		   msg.setContent(body, "text/html");
+		   msg.setSentDate(new Date());
+		   
+		   Transport.send(msg);   
+		}
+}
