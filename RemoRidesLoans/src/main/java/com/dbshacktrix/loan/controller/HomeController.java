15fc1be73aa449c2275386e30f82bb2d55514c34
Aneesh Bansal <contact2aneesh@gmail.com>
2019-03-30 13:46:27 +0530

    Adding RestController

--- RemoRidesLoans/src/main/java/com/dbshacktrix/loan/controller/HomeController.java
+++ RemoRidesLoans/src/main/java/com/dbshacktrix/loan/controller/HomeController.java
@@ -10,7 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/RemoRides")
 public class HomeController {
 	
 	@Autowired
