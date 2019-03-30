42b814e65794c3fea6a540cf79247383dce0f0d2
gouthamrayaprolu <48426007+gouthamrayaprolu@users.noreply.github.com>
2019-03-30 13:57:04 +0530

    commit

---
+++ RemoRidesLoans/src/main/java/com/dbshacktrix/loan/model/EMIModel.java
@@ -0,0 +1,73 @@
+package com.dbshacktrix.loan.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class EMIModel {
+
+	@Id
+	@GeneratedValue
+	private int eid;
+
+	public int getEid() {
+		return eid;
+	}
+
+	private int month;
+	private double interest;
+	private double principal;
+	private double emi;
+	private double balance;
+	private int uid;
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public double getInterest() {
+		return interest;
+	}
+
+	public void setInterest(double interest) {
+		this.interest = interest;
+	}
+
+	public double getPrincipal() {
+		return principal;
+	}
+
+	public void setPrincipal(double principal) {
+		this.principal = principal;
+	}
+
+	public double getEmi() {
+		return emi;
+	}
+
+	public void setEmi(double emi) {
+		this.emi = emi;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+}
