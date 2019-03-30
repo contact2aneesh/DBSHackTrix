4859fd2acda2c641925c3e21d7a2b82c0cc27056
gouthamrayaprolu <48426007+gouthamrayaprolu@users.noreply.github.com>
2019-03-30 12:27:32 +0530

    commit

--- RemoRidesLoans/src/main/java/com/dbshacktrix/loan/repo/LOANPLANRepo.java
+++ RemoRidesLoans/src/main/java/com/dbshacktrix/loan/repo/LOANPLANRepo.java
@@ -1,5 +1,18 @@
 package com.dbshacktrix.loan.repo;
 
-public interface LOANPLANRepo {
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.dbshacktrix.loan.model.LOANPLAN;
+
+public interface LOANPLANRepo extends JpaRepository<LOANPLAN, String>{
+
+	List<LOANPLAN> findBySlabs(BigDecimal amount);
+	
+	@Query("from LOANPLAN where slabs >=?1")
+	List<LOANPLAN> findBySlabsSorted(BigDecimal amount);
 
 }
